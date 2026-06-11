d7297023900ec54e1d3eb063aac55c18ca26a461
dashfire-jpg <dashfire@gmail.com>
2026-06-11 19:08:10 -0300

    fix: rewrite all corrupted handlers

---
+++ api/backend/handlers/bi.ts
@@ -0,0 +1,6 @@
+﻿import { Request, Response } from "express";
+import { prisma } from "../../prisma/client.js";
+
+export default async function biHandler(req: Request, res: Response) {
+  return res.json({ success: true, data: [] });
+}
